347d89f31e517c9e7b49817e64dc63ebed41727e
bascoe10 <bassco10@gmail.com>
2020-12-15 21:16:40 -0800

    complete day 15 p1

--- 15/day15p1.py
+++ 15/day15p1.py
@@ -1,19 +1,23 @@
-from collections import OrderedDict
-
 def main() -> None:
-    # starting = {9: [], 19: [], 1: [], 6: [], 0: [], 5: [], 4: [], }
-    #starting = OrderedDict({1: [1], 3: [2], 2: [3]})
-    starting = OrderedDict({0: [1], 3: [2], 6: [3]})
-    numbers_spoken = starting.keys()[:]
-    last_number = None
-    for _ in range(2020):
-        last_number = starting.keys()[-1]
-        if len(starting[last_number] == 1):
-            numbers_spoken.append(0)
+    starting = {9: [1], 19: [2], 1: [3], 6: [4], 0: [5], 5: [6], 4: [7]}
+    numbers_spoken = [9, 19, 1, 6, 0, 5, 4]
+
+    last_number = numbers_spoken[-1]
+    sn = 2020 - len(numbers_spoken)
+
+    for _ in range(sn):
+        if len(starting[last_number]) == 1:
+            last_number = 0
+        else:
+            pult, ult = starting[last_number][-2:]
+            last_number = ult - pult
+        numbers_spoken.append(last_number)
+        if last_number in starting:
             starting[last_number].append(len(numbers_spoken))
         else:
-            ult, pult = starting[last_number]
+            starting[last_number] = [len(numbers_spoken)]
 
+    print(f'The 2020th entry is --> {last_number}')
 
 
 if __name__ == '__main__':
